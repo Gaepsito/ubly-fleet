4a0197c6b03ca91b6b774d681c932967439c7520
Gaepsito <paul.arellano@outlook.es>
2025-06-01 21:37:53 -0500

    Add files via upload

---
+++ pages/Fleet.js
@@ -0,0 +1,10 @@
+import FleetList from '../components/FleetList';
+
+export default function Fleet() {
+  return (
+    <div>
+      <h1>Gestión de Flotas</h1>
+      <FleetList />
+    </div>
+  );
+}
